057869fb4437a70b5cc36bf26e7d7b58bfcd38fc
deha24 <127858504+deha24@users.noreply.github.com>
2025-03-19 12:55:31 +0100

    Shell sort done

--- main.py
+++ main.py
@@ -9,6 +9,24 @@ translating and running the source code on-the-fly during execution.
 from HelloWorld import HelloWorld
 
 # Checks if the Python script is being run as the main program (not imported as a module)
-if __name__ == "__main__":
-    print(HelloWorld())
+def shell_sort(arr):
+    length = len(arr)
+    gap = [1]
+    k=0
+    while True:
+        tmp=(4**(k+1))+(3*(2**k))+1
+        if tmp<=length:
+            gap.append(tmp)
+        else:
+            break
+        k+=1
+    for i in range(len(gap)-1,-1,-1):
+        for j in range(0,length,gap[i]):
+            key=arr[j]
+            k=j-gap[i]
+            while k>=0 and arr[k]>key:
+                arr[k+gap[i]]=arr[k]
+                k-=gap[i]
+            arr[k+gap[i]]=key
+    return arr
     
