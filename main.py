68c32f0af9feb6ff5b528d22bff4b2f572654c6e
QbaRudy <rozmiarek14@gmail.com>
2025-03-19 22:40:01 +0100

    quickSort pt1

--- main.py
+++ main.py
@@ -78,3 +78,14 @@ def selectionSort(arr):
             if arr[j] < arr[min_index]:
                 min_index = j
             arr[i], arr[min_index] = arr[min_index], arr[i]
+
+def divqSort():
+    pivot = array[high]
+    i = low - 1
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            (array[i], array[j]) = (array[j], array[i])
+
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    return i + 1
