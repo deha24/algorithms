103420056055c8fa16d5556de33fd2bbdb6f4bf9
QbaRudy <rozmiarek14@gmail.com>
2025-03-19 21:35:21 +0100

    selectionSort pt2

--- main.py
+++ main.py
@@ -74,4 +74,6 @@ def selectionSort(arr):
     length = len(arr)
     for i in range(length):
         min_index = i
-        
+        for j in range(i+1, length):
+            if arr[j] < arr[min_index]:
+                min_index = j
