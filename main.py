4944379ddc5445dcd137f4b51e8c1f92ef48399a
QbaRudy <rozmiarek14@gmail.com>
2025-03-19 20:56:19 +0100

    insertionSort done

--- main.py
+++ main.py
@@ -62,3 +62,10 @@ def insertionSort(arr):
     length = len(arr)
     if length <= 1:
         return
+    for i in range(1, length):
+        key = arr[i]
+        j = i - 1
+        while j>=0 and key<arr[j]:
+            arr[j+1] = arr[j]
+            j = j - 1
+        arr[j+1] = key
